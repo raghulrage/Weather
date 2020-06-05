f63c7eacac884a07e58797ae12558c9bbf64cd24
raghulrage <46341793+raghulrage@users.noreply.github.com>
2020-06-05 12:13:51 +0530

    Added content

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+#import packages
 import requests
 import json
 
 class Weather:
-    def __init__(self, name, status, description, temperature, humidity, pressure, visibility, longitude, latitude, wind):
+    
+    '''Weather attributes'''
+    
+    def __init__(self, name, status, description, temperature, humidity, pressure, longitude, latitude, wind):
         
         self.cityName = name
         self.status = status
@@ -10,16 +14,17 @@ class Weather:
         self.temperature = temperature
         self.humidity = humidity
         self.pressure = pressure
-        self.visibility = visibility
         self.longitude = longitude
         self.latitude = latitude
         self.wind = wind
 
+    '''Display weather details'''
+    
     def displayWeatherData(self):
-
+        
         print('\n'+'*'*10,'Weather in',self.cityName,'*'*10+'\n')
         print()
-        print('Temperature:',self.temperature-273.15,u'\u2103')
+        print('Temperature:',round(self.temperature-273.15,2),u'\u2103')
         print()
         print('Humidity:',self.humidity,'%')
         print()
@@ -27,24 +32,29 @@ class Weather:
         print()
         print('Wind:',self.wind,'m/s')
         print()
-        print('Visibility:',self.visibility,'meter')
-        print()
         print('Weather status:',self.status)
         print()
         print('Description:\n'+' '*5+'*',self.statusDescription.title())
         print()
         print('Coordinates:\n'+' '*5+'* Longitude:',self.longitude,'\n'+' '*5+'* Latitude:',self.latitude)
-        
-if __name__ == '__main__':
+
+#Get data from open weather app 
+def getLink():
+    
+    city = input('\n\nEnter City name: ')
     
     url = "http://api.openweathermap.org/data/2.5/weather"
-
     apiKey = '1e67bf59e9e77cca70509fa5dd4fe27a'
-
-    city = input('Enter City name: ')
-
     link = url+'?q='+city+'&appid='+apiKey
 
+    return link
+
+#main function
+def main():
+    
+    link = getLink()
+
+    #handle exception
     try:
         response = requests.request("GET", link)
 
@@ -55,13 +65,39 @@ if __name__ == '__main__':
             dataWeather = data['weather'][0]
             dataTemp = data['main']
             dataCoord = data['coord']
-            
-            weather = Weather(data['name'], dataWeather['main'], dataWeather['description'], dataTemp['temp'], dataTemp['humidity'], dataTemp['pressure'], data['visibility'], dataCoord['lon'], dataCoord['lat'],data['wind']['speed'])
+
+            weather = Weather(data['name'], dataWeather['main'], dataWeather['description'], dataTemp['temp'], dataTemp['humidity'], dataTemp['pressure'], dataCoord['lon'], dataCoord['lat'],data['wind']['speed'])
 
             weather.displayWeatherData()
             
         else:
             print('Invalid City Name')
             
+    except requests.exceptions.ConnectionError:
+        
+        print('\nNo Internet.')
+        print('Try:\n\t* Checking the network cables, modem, and router\n\t* Reconnecting to Wi-Fi\n\t* Running Windows Network Diagnostics')
+
     except Exception as e:
-        print('Error Occured, Error:',e)
+        print('Oops!',e.__class__,'occured.',e)
+
+        
+if __name__ == '__main__':
+
+    print('*'*15,'Weather Forcast','*'*15)
+
+    main()
+
+    
+    while True:
+        option = input('\nDo you want to continue[Y/N]: ').lower()
+        
+        if option == 'y':
+            main()
+        else:
+            print('\nThank You.')
+            break
+    
+
+
+
